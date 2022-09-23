3e1730bdad52cd3e9c0952a48feeac6198f11a5d
Steve Braaksma <stevebraaksma@hotmail.com>
2022-09-22 21:48:52 -0500

    correct post route

--- server.js
+++ server.js
@@ -3,26 +3,24 @@ const express = require('express');
 const app = express();
 const methodOverride = require('method-override');
 
-
-
 // listener
 app.listen(3000, () => console.log(`server is listening on port 3000`));
 
-
-
 // middleware
 app.use(express.urlencoded({ extended: false }));
-
-
-
 
 let userTransactions = [];
 let totalPointsAdded = 0;
 let totalPointsSpent = 0;
 
-
-// home
+// home route
 app.get('/', (req, res) => {
     res.render('home.ejs');
     console.log('route hit');
+});
+
+// add route
+app.post('/add', (req, res) => {
+    res.render('home.ejs');
+    console.log('hit');
 });
